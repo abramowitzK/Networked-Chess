c44d85874afa20bd1b497884d4297fd3a97699d6
Micah Chao <chaom1994@gmail.com>
2016-02-17 02:24:20 -0500

    added scene to check that users are ready to begin

---
+++ ChessUI/src/javafx/MatchedController.java
@@ -0,0 +1,58 @@
+package javafx;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MatchedController {
+
+	@FXML Button beginButton;
+	@FXML Button leaveButton;
+	
+	public void handleBegin(){
+		System.out.println("Clicked on Begin");
+		
+		// Transitions the UI to the GameBoard and loads the GameBoard FXML and CSS file
+		Stage getstage = (Stage) beginButton.getScene().getWindow();
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("GameBoard.css").toExternalForm());
+			
+			getstage.setScene(scene);
+			getstage.sizeToScene();
+			getstage.show();
+			System.out.println("Switch to game board");
+			
+			// TODO Add background task to send to server and wait for other person to press begin
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void handleLeave(){
+		System.out.println("Clicked on Leave");
+		try{
+			Stage getstage = (Stage) leaveButton.getScene().getWindow();
+			Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+			
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("MainMenu.css").toExternalForm());
+			
+			getstage.setScene(scene);
+			getstage.show();
+			System.out.print("Switch to Main Menu");
+			
+			//TODO Remove player from queue and notify other user that they left
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+}
